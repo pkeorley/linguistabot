6d9e3be0a6de7d859a4bdf20a03edbd38a7d6be2
pkeorley <nerfinyt@gmail.com>
2023-07-26 16:41:28 +0200

    feat: 1.0.2 - change in architecture

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+import os
+
 from disnake.ext import commands
 from database import Database
 
@@ -8,3 +10,13 @@ class Linguista(commands.Bot):
             command_prefix=","
         )
         self.database = Database(self)
+
+    def load_extensions(self) -> None:
+        for filename in os.listdir("cogs"):
+            try:
+                if filename.endswith(".py"):
+                    self.load_extension(f"cogs.{filename[:-3]}")
+                    print("✅ " + filename)
+            except Exception as error:
+                print(f"⛔ {filename} | Exception: {error}")
+        print("The cogs is finally loaded!")
--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-import os
-
 from config import datas
 from client import Linguista
 
 
 bot = Linguista()
-
-
-for filename in os.listdir("cogs"):
-    try:
-        if filename.endswith(".py"):
-            bot.load_extension(f"cogs.{filename[:-3]}")
-    except Exception as error:
-        print(error)
-
-
+bot.load_extensions()
 bot.run(datas["TOKEN"])
