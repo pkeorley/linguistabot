7dec53fcfeceaeadddfb064da2118e89d80c71bf
pkeorley <nerfinyt@gmail.com>
2023-07-26 16:46:13 +0200

    feat: 1.0.2 - Version update and added supported languages for context.reverso.net

--- config.py
+++ config.py
@@ -1,5 +1,25 @@
 datas = {
     "TOKEN": "",
     "MONGO_URL": "",
-    "BOT_VERSION": "1.0.1"
+    "BOT_VERSION": "1.0.2"
+}
+
+LANGUAGES = {
+    'ar': 'arabic',
+    'de': 'german',
+    'en': 'english',
+    'es': 'spanish',
+    'fr': 'french',
+    'he': 'hebrew',
+    'it': 'italian',
+    'ja': 'japanese',
+    'ko': 'korean',
+    'nl': 'dutch',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sv': 'swedish',
+    'tr': 'turkish',
+    'uk': 'ukrainian'
 }
